2a07d9ddd385935e5c13699c37e40c0b5eaf712b
Zean Zhou <zeanzhou@seas.upenn.edu>
2020-06-19 01:28:47 -0400

    Add argument parser to server

--- udping-server.py
+++ udping-server.py
@@ -1,3 +1,4 @@
+import argparse
 import socket, struct, time, random
 from packet import *
 from ctrl_c_handler import set_signal_handler
@@ -6,8 +7,20 @@ def signal_handler(signum, frame=None):
     exit(1)
 set_signal_handler(signal_handler)
 
+# Parse argument
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', metavar='server-address', default='0.0.0.0', help='Server address to bind. IPv4/IPv6 address. The default is 0.0.0.0')
+parser.add_argument('-p', metavar='server-port', type=int, default=11888, help='Port to listen on. UDPing client must ping this port. The default is 11888.')
+parser.add_argument('-v', action="store_true", help='Verbose mode. Display each UDPing request. The default is off.')
+namespace = parser.parse_args()
+args_dict = vars(namespace)
+
+addr = args_dict['b']
+port = args_dict['p']
+is_verbose = args_dict['v']
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('0.0.0.0', 11888))
+s.bind((addr, port))
 
 buffer = bytearray(65536)
 buffer_view = memoryview(buffer)
@@ -17,6 +30,7 @@ while True:
         UDPingPacket.response_ping(buffer_view)
         s.sendto(buffer_view[:nbytes], addr)
         packet = UDPingPacket.from_bytes(buffer_view, nbytes)
-        print(f'Receiving from {addr[0]}:{addr[1]} seq={packet.index} nbytes={len(packet.payload)}({nbytes})')
+        if is_verbose:
+            print(f'Receiving from {addr[0]}:{addr[1]} seq={packet.index} nbytes={len(packet.payload)}({nbytes})')
     except ConnectionResetError as e:
         pass
